03ebb24a512d654da6a63a64ec285df489e8b844
Haim Raitsev <haim@oneaudience.com>
2018-11-14 15:47:36 +0200

    add travis file

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -79,7 +79,6 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then(user => {
     // Check for user
     if (!user) {
-      console.log("asdasd");
       errors.email = "User not found";
       return res.status(404).json(errors);
     }
