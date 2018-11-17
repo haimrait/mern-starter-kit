b860927a8f8586e44ceff0a2abdad4a8edf0ba94
DESKTOP-MOOGR13\haimr <haimrait@gmail.com>
2018-11-17 19:22:58 +0200

    add some changes

--- server/server.js
+++ server/server.js
@@ -36,14 +36,14 @@ app.use("/users", users);
 app.use("/channels", channels);
 
 // Server static assets if in profuction
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 const port = process.env.PORT || 5000;
 
